72da12d03ca8f219ee7ed08ff0c4ed118c255767
ashwin <ashwinathrey93@gmail.com>
2018-05-14 10:27:04 -0400

    Logout working

--- src/app/providers/af.service.ts
+++ src/app/providers/af.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import {AngularFireAuth} from 'angularfire2/auth';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireAuthModule,AngularFireAuth} from 'angularfire2/auth';
 import {Observable} from 'rxjs/Observable';
-
+import {User} from 'firebase';
 import * as firebase from 'firebase/app';
  
 
