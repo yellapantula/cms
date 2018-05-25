925a51290798f91ffdedea2aafb7f05f7e42d218
ashwin <ashwinathrey93@gmail.com>
2018-05-25 15:32:08 -0400

    Add Front Page module and changed routing structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {environment} from './../environments/environment';
-
+import {AfService} from './providers/af.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,10 @@ import {AngularFireDatabaseModule,AngularFireDatabase} from 'angularfire2/databa
 import {AppRoutingModule} from './app-routing-module';
 import { AppComponent } from './app.component';
 import {environment} from '../environments/environment';
-import { PagesListComponent } from './pages-list/pages-list.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import {AfService} from './providers/af.service';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFirestore, AngularFirestoreModule} from 'angularfire2/firestore';
-import { AppNavbarComponent } from './app-navbar/app-navbar.component';
-import { HomePageComponent } from './home-page/home-page.component';
 import {AdminGuard} from './guards/admin.guard';
 import {SubscriberGuard} from './guards/subscriber.guard';
 import {MatButtonModule,MatToolbarModule,MatIconModule} from '@angular/material';
@@ -22,10 +19,9 @@ import {PostsService} from './service/posts/posts.service';
 @NgModule({
   declarations: [
     AppComponent,
-    PagesListComponent,
     LoginPageComponent,
-    AppNavbarComponent,
-    HomePageComponent,
+  
+    
     
     
    
